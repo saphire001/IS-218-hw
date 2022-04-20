9c29624b1faffc2d7a46d072bd9457e7b60bdd48
saanikajoshi <spj29@njit.edu>
2022-04-20 00:34:59 -0400

    html files

--- app/User_Management/__init__.py
+++ app/User_Management/__init__.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, flash,current_app
+from flask import Blueprint, render_template, redirect, url_for, flash, current_app
 from flask_login import login_required, current_user
 from app.auth.decorators import admin_required
 from werkzeug.security import generate_password_hash
